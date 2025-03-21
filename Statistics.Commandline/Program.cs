b42db3ee30e2776db899739b2bccf7b5c68e22a7
André Steenhoff Madsen <andre@steenhoff.dk>
2025-03-21 16:18:56 +0100

    feature: implements logic that should make commandline call ai's on startup, if there are any configured.

--- Statistics.Commandline/Program.cs
+++ Statistics.Commandline/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.Extensions.Hosting;
 using Statistics.Shared.Persistence;
 using Microsoft.Extensions.DependencyInjection;
+using Statistics.Shared.Abstraction.Interfaces.Persistence;
+using Statistics.Shared.Abstraction.Interfaces.Services;
+using Statistics.Shared.Models.Entity;
+using Statistics.Shared.Models.Searchable;
 
 namespace Statistics.Commandline;
 
 internal class Program
 {
+    private CommandlineStartup Startup { get; init; }
+
     private static void Main(string[] args)
     {
         var program = new Program();
@@ -16,16 +22,66 @@ internal class Program
 
     private async Task Run(string[] args)
     {
-        var startup = new CommandlineStartup();
+        var Startup = new CommandlineStartup();
 
         HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-        startup.SetupApplication(builder);
-        startup.SetupServices(builder.Services);
+        Startup.SetupApplication(builder);
+        Startup.SetupServices(builder.Services);
 
         using IHost host = builder.Build();
         Console.WriteLine("Host has been build...");
 
+        await ExecuteAllPrompts();
+
         await host.RunAsync();
+    }
+
+    private async Task ExecuteAllPrompts()
+    {
+        GetServicesOrThrow(out var promptEntityService, out var aiEntityService, out var responseEntityService,
+            out IMasterArtificialIntelligencePromptService masterAiService);
+
+        var allAis = await aiEntityService.GetEntities(new SearchableArtificialIntelligence());
+        var artificialIntelligences = allAis.ToList();
+        if (!artificialIntelligences.Any())
+        {
+            return;
+        }
+
+        var allPrompts = await promptEntityService.GetEntities(new SearchablePrompt());
+        var prompts = allPrompts.ToList();
+        if (!prompts.Any())
+        {
+            return;
+        }
+
+        var aiResponses = await masterAiService.PromptSuppliedAis(artificialIntelligences, prompts);
+        var responses = aiResponses.ToList();
+        if (!responses.Any())
+        {
+            return;
+        }
+
+        await responseEntityService.AddEntities((IEnumerable<Response>) responses.AsEnumerable());
+    }
+
+    private void GetServicesOrThrow(
+        out IEntityQueryService<Prompt, SearchablePrompt> promptEntityService,
+        out IEntityQueryService<ArtificialIntelligence, SearchableArtificialIntelligence> aiEntityService,
+        out IEntityQueryService<Response, SearchableResponse> responseEntityService,
+        out IMasterArtificialIntelligencePromptService masterAiService)
+    {
+        promptEntityService = Startup.ServiceProvider.GetService<IEntityQueryService<Prompt, SearchablePrompt>>() ??
+                              throw new InvalidOperationException();
+        aiEntityService = Startup.ServiceProvider
+                              .GetService<
+                                  IEntityQueryService<ArtificialIntelligence, SearchableArtificialIntelligence>>() ??
+                          throw new InvalidOperationException();
+        responseEntityService =
+            Startup.ServiceProvider.GetService<IEntityQueryService<Response, SearchableResponse>>() ??
+            throw new InvalidOperationException();
+        masterAiService = Startup.ServiceProvider.GetService<IMasterArtificialIntelligencePromptService>() ??
+                          throw new InvalidOperationException();
     }
 }
